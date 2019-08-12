4196a8289fb35a426a4d427a059fabeae0becc87
xuekai <ee_shadow@163.com>
2019-08-12 10:38:31 +0800

    update: .gitignore

--- docs/docs/.vuepress/config.js
+++ docs/docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    base: '/Docs/SoonSpace/',
+    base: '/xunwei-web/SDK/SoonSpace/Docs/',
     title: 'SoonSpace-docs',
     description: '智慧城市可视化管理平台-迅维智能科技主要研究建筑信息化和智能化，有智慧建筑、智慧园区、智慧安防、智慧消防、特色小镇、物联网、资产可视化管理平台等。',
     configureWebpack: {
